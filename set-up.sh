e32701455d73a8dda4d03b5986d43e08b142b183
Carlo Yague <carlo.ankou@gmail.com>
2018-02-01 13:20:21 +0100

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -9,6 +9,7 @@ echo 'rename chromosomes...'
 gunzip data/ce10_bowtie2_index/genome.fa.gz
 sed 's/CHROMOSOME_/chr/g' data/ce10_bowtie2_index/genome.fa | sed 's/chrMtDNA/chrM/g' > data/ce10_bowtie2_index/temp.fa
 mv data/ce10_bowtie2_index/temp.fa data/ce10_bowtie2_index/genome.fa
+rm data/ce10_bowtie2_index/temp.fa
 echo 'build bowtie2 index...'
 bowtie2-build data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
 echo 'build samtools index...'
