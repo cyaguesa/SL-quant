401fb0139bcc97971c9d5afc7ee3af1be63c3419
Carlo Yague <carlo.ankou@gmail.com>
2018-02-01 12:13:26 +0100

    added a line to rename chromosomes

--- set-up.sh
+++ set-up.sh
@@ -4,8 +4,10 @@ mkdir data/ce10_bowtie2_index
 mkdir SL-quant_results
 echo 'download C. elegans genome...'
 curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic.fa.gz -o data/ce10_bowtie2_index/genome.fa.gz
-echo 'gunzip it...'
+echo 'rename chromosomes...'
 gunzip data/ce10_bowtie2_index/genome.fa.gz
+sed 's/CHROMOSOME_/chr/g' data/ce10_bowtie2_index/genome.fa | sed 's/chrMtDNA/chrM/g' > data/ce10_bowtie2_index/temp.fa
+mv data/ce10_bowtie2_index/temp.fa data/ce10_bowtie2_index/genome.fa
 echo 'build bowtie2 index...'
 bowtie2-build data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
 echo 'build samtools index...'
