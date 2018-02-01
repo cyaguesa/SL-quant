af7ecfcf992d753276dcf390a84440bf589ec842
Carlo Yague <carlo.ankou@gmail.com>
2018-02-01 11:02:29 +0100

    replaced wget by curl as it is more portable

--- set_up.sh
+++ set_up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'download C. elegans genome...'
-wget ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic.fa.gz -P data/ce10_bowtie2_index
+mkdir data/ce10_bowtie2_index
+curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic.fa.gz -o data/ce10_bowtie2_index/genome.fa.gz
 echo 'gunzip it...'
-gunzip data/ce10_bowtie2_index/c_elegans.WS220.genomic.fa.gz
-mv data/ce10_bowtie2_index/c_elegans.WS220.genomic.fa data/ce10_bowtie2_index/genome.fa
+gunzip data/ce10_bowtie2_index/genome.fa.gz
 echo 'build bowtie2 index...'
 bowtie2-build data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
 echo 'build samtools index...'
