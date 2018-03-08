02b108a9d6759d2921dd67adad9d91b05a71ac35
Carlo Yague <carlo.ankou@gmail.com>
2018-03-08 16:30:31 +0100

    replaced tophat2 by hisat2

--- map_reads.sh
+++ map_reads.sh
@@ -5,7 +5,7 @@
 # instructions on https://github.com/cyaguesa/SL-quant
 
 # REQUIREMENTS (see installation instruction if unmet):      VERSION USED 
-# - tophat2 installed and in your path                       2.1.1
+# - hisat2 installed and in your path                        2.0.5             
 # - samtools installed and in your path                      1.5
 
 # by Carlo Yague-Sanz, 2017
@@ -15,11 +15,12 @@
 #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 dir="data/reads/SRR1585277"            # data directory
-index="data/ce10_bowtie2_index/genome" # genome index file
+index="data/ce10_hisat2_index/genome"  # genome index file
 offset=12                              # length of "_1.fastq.gz" +1. When the ".R1.fastq.gz" notation is used, this should be set to 13.
 core=1                                 # number of cores used for sorting
 mem="3328M"                            # amount of memory by core used for sorting
 set -e                                 # stops script at the first error
+threads=4                              # number of threads to use for mapping.
 
 #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
@@ -29,7 +30,9 @@ for i in $(ls $dir/*.fastq.gz | rev | cut -c $offset- | rev | uniq)
   echo ""; echo "fastq file : ${i}"
 
   echo "... aligning..."
-  tophat --rg-sample ${i} --rg-id ${i} -r 200 --no-sort --mate-std-dev 150 -N 2 --read-edit-dist 2 --min-intron-length 5 --max-intron-length 3000 --max-multihits 4 --no-discordant --no-coverage-search -p 4 --library-type fr-firststrand --output-dir ${i} $index ${i}_1.fastq.gz ${i}_2.fastq.gz
+  hisat2 -p $threads --no-softclip --no-discordant --min-intronlen 20 --max-intronlen 5000 --rna-strandness FR -x $index -1 ${i}_1.fastq.gz -2 ${i}_2.fastq.gz -S ${i}/mapped.sam
+  samtools view -b -F 4 ${i}/mapped.sam> ${i}/accepted_hits.bam
+  samtools view -b -f 4 ${i}/mapped.sam> ${i}/unmapped.bam
 
   echo "... done... sorting..."
   samtools sort -@ $core -m $mem ${i}/accepted_hits.bam > ${i}/accepted_hits_sorted.bam
@@ -39,6 +42,7 @@ for i in $(ls $dir/*.fastq.gz | rev | cut -c $offset- | rev | uniq)
   samtools index ${i}/accepted_hits_sorted.bam
   samtools flagstat ${i}/accepted_hits_sorted.bam > ${i}/accepted_hits.bam.flagstat
   rm ${i}/accepted_hits.bam
+  rm ${i}/mapped.sam
 
   echo "... done. Have a nice day !"
   done
