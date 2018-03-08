abb05163a2482763fc93508c830b474894001535
Carlo Yague <carlo.ankou@gmail.com>
2018-03-08 10:36:51 +0100

    added build hisat2 index

--- set-up.sh
+++ set-up.sh
@@ -12,6 +12,8 @@ mv data/ce10_bowtie2_index/temp.fa data/ce10_bowtie2_index/genome.fa
 rm data/ce10_bowtie2_index/temp.fa
 echo 'build bowtie2 index...'
 bowtie2-build --threads 4 data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
+echo 'build hisat2 index...'
+hisat2-build
 echo 'build samtools index...'
 samtools faidx data/ce10_bowtie2_index/genome.fa
 echo 'done ! You can now test SL-quant !'
