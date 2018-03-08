3424b2f129dbbcce6a6d02187dfb8060df336e54
Carlo Yague <carlo.ankou@gmail.com>
2018-03-08 11:35:29 +0100

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -2,6 +2,7 @@
 
 chmod +x SL-quant.sh map_reads_modENCODE.sh map_reads.sh
 mkdir data/ce10_bowtie2_index
+mkdir data/ce10_hisat2_index
 mkdir SL-quant_results
 echo 'download C. elegans genome...'
 curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic.fa.gz -o data/ce10_bowtie2_index/genome.fa.gz
@@ -13,7 +14,7 @@ rm data/ce10_bowtie2_index/temp.fa
 echo 'build bowtie2 index...'
 bowtie2-build --threads 4 data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
 echo 'build hisat2 index...'
-hisat2-build
+hisat2-build -p 4 data/ce10_bowtie2_index/genome.fa data/ce10_hisat2_index/genome
 echo 'build samtools index...'
 samtools faidx data/ce10_bowtie2_index/genome.fa
 echo 'done ! You can now test SL-quant !'
