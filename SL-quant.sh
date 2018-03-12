fa5f2ee1114baac0472d455af26631148eb9ce48
Carlo Yague <carlo.ankou@gmail.com>
2018-03-12 13:02:37 +0100

    typo correction

--- SL-quant.sh
+++ SL-quant.sh
@@ -93,8 +93,8 @@ if [ "$SINGLE" != "single" ]; then
     blastn -query ${3}_oneEnd_unmapped.fasta -task blastn -db $SL_db  -outfmt 6 -max_target_seqs 1 -num_threads 4 -word_size 8 > ${3}_blasted.txt 2>${3}_log.txt
 
     echo "   done... filter significant alignment with qstart==1..."
-    awk '$7 == 1 && $10 >= "'"$send"'" && $11 < "0.05" {print $0}' ${2}_blasted.txt | grep "SL1" > ${2}_blasted_SL1.txt
-    awk '$7 == 1 && $10 >= "'"$send"'" && $11 < "0.05" {print $0}' ${2}_blasted.txt | grep "SL2" > ${2}_blasted_SL2.txt
+    awk '$7 == 1 && $10 >= "'"$send"'" && $11 < "0.05" {print $0}' ${3}_blasted.txt | grep "SL1" > ${3}_blasted_SL1.txt
+    awk '$7 == 1 && $10 >= "'"$send"'" && $11 < "0.05" {print $0}' ${3}_blasted.txt | grep "SL2" > ${3}_blasted_SL2.txt
 
     echo "   done... retrieve mapped mates..."
 
