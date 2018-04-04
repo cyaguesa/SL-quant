32e0da76fce2801a8fd652ba293a141233b24388
Carlo Yague <carlo.ankou@gmail.com>
2018-04-04 09:06:55 +0200

    change to WS262 annotation
    for consistency

--- set-up.sh
+++ set-up.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
 chmod +x SL-quant.sh map_reads_modENCODE.sh map_reads.sh
-mkdir data/ce10_bowtie2_index
 mkdir data/ce10_hisat2_index
 mkdir SL-quant_results
+echo ""
+
+echo 'download C. elegans annotation...'
+curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS262/species/c_elegans/PRJNA13758/c_elegans.PRJNA13758.WS262.canonical_geneset.gtf.gz -o data/genes.gtf.gz
+gunzip data/genes.gtf.gz
+echo ""
+
 echo 'download C. elegans genome...'
-curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic.fa.gz -o data/ce10_bowtie2_index/genome.fa.gz
+curl ftp://ftp.wormbase.org/pub/wormbase/releases/WS262/species/c_elegans/PRJNA13758/c_elegans.PRJNA13758.WS262.canonical_geneset.gtf.gz -o data/ce10_hisat2_index/genome.fa.gz
+gunzip data/ce10_hisat2_index/genome.fa.gz
+echo ""
+
 echo 'rename chromosomes...'
-gunzip data/ce10_bowtie2_index/genome.fa.gz
-sed 's/CHROMOSOME_/chr/g' data/ce10_bowtie2_index/genome.fa | sed 's/chrMtDNA/chrM/g' > data/ce10_bowtie2_index/temp.fa
+sed 's/CHROMOSOME_/chr/g' data/ce10_hisat2_index/genome.fa | sed 's/chrMtDNA/chrM/g' > data/ce10_hisat2_index/temp.fa
 mv data/ce10_bowtie2_index/temp.fa data/ce10_bowtie2_index/genome.fa
 rm data/ce10_bowtie2_index/temp.fa
-echo 'build bowtie2 index...'
-bowtie2-build --threads 4 data/ce10_bowtie2_index/genome.fa data/ce10_bowtie2_index/genome
+echo ""
+
 echo 'build hisat2 index...'
-hisat2-build -p 4 data/ce10_bowtie2_index/genome.fa data/ce10_hisat2_index/genome
+hisat2-build -p 4 data/ce10_hisat2_index/genome.fa data/ce10_hisat2_index/genome
+echo ""
+
 echo 'build samtools index...'
 samtools faidx data/ce10_bowtie2_index/genome.fa
+echo ""
+
 echo 'build blast database...'
 makeblastdb -dbtype nucl -in data/blast_db/SL.fasta
+echo ""
+
 echo 'done ! You can now test SL-quant !'
